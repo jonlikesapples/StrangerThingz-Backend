04b743f0bea6cb90cb73d1a849e10e1bdc0ee58c
jonlikesapples <jonlikesapples@gmail.com>
2018-03-07 18:14:38 -0800

    Moved from python-firebase to pyrebase

--- app.py
+++ app.py
@@ -1,34 +1,50 @@
 from flask import Flask, jsonify
 import json
-import requests
-from firebase import firebase
+import pyrebase
+
 app = Flask(__name__)
 
+#https://github.com/thisbejim/Pyrebase
 
-#https://github.com/ozgur/python-firebase
-fb = firebase.FirebaseApplication('https://stranger-things-ce12a.firebaseio.com/', None)
+config = {
+  "apiKey": "AIzaSyAb0csAFZDQoYIJrflFTuYAwx7rS1t3oYg",
+  "authDomain": "stranger-things-ce12a.firebaseapp.com",
+  "databaseURL": "https://stranger-things-ce12a.firebaseio.com/",
+  "storageBucket": "stranger-things-ce12a.appspot.com",
+}
+
+firebase = pyrebase.initialize_app(config)
+
+db = firebase.database()
 
 #get from firebase server
 @app.route("/", methods=['GET'])
 def get():
-	queryResult = fb.get('/users', None) #returns a type List
-	jsonresult = jsonify(queryResult) #jsonify it
-	return jsonresult
+	# queryresult = db.child("users").get().val() #type orderedDict
+	# data = json.loads(json.dumps(queryresult)) #type (regular) dict
+	# stringdata = json.dumps(data) #type string, no real use for this
+	QUERY_RESULT = ""
+	dbresult = db.child("users").get() #type orderedDict
+	for user in dbresult.each(): 
+		QUERY_RESULT += "key: " + user.key() + " " + "val: " + user.val() + "\n"
+	return QUERY_RESULT
 
 @app.route('/post')
 def redirect():
-	return "please go to /post/<param>"
+	data = {"name": "Mortimer 'Morty' Smith"}
+	db.child("users").push(data)
+	return "hi"
 
 @app.route("/post/<param>",)
 def post(param):
-	posted = fb.post('/users', param)
-	key = posted["name"] #key for posted thing
-	return param + " was posted."
+	
+	return key
+	#return param + " was posted."
 
 @app.route("/delete")
 def delete():
-	KEY = "2" 
-	return fb.delete('/users', KEY)
+
+	return 
 
 if __name__ == '__main__':
     app.run(debug=True)
