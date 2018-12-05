920d7aecb8ccebaff09b19847ec184a64698f0f5
jonlikesapples <jonlikesapples@gmail.com>
2018-12-05 11:52:15 -0800

    adding utils to heroku

---
+++ utils/responses.py
@@ -0,0 +1,86 @@
+from flask import make_response, jsonify
+
+INVALID_FIELD_NAME_SENT_422 = {
+    "http_code": 422,
+    "code": "invalidField",
+    "message": "Not all field names are valid."
+}
+
+INVALID_INPUT_422 = {
+    "http_code": 422,
+    "code": "invalidInput",
+    "message": "Invalid input"
+}
+
+MISSING_PARAMETERS_422 = {
+    "http_code": 422,
+    "code": "missingParameter",
+    "message": "Missing parameters."
+}
+
+UNAUTHORIZED_401 = {
+    "http_code": 401,
+    "code": "authorized, but invalid credentials",
+    "message" : "authorized, but invalid credentials"
+}
+
+BAD_REQUEST_400 = {
+    "http_code": 400,
+    "code": "badRequest",
+    "message": "Bad request"
+}
+
+SERVER_ERROR_500 = {
+    "http_code": 500,
+    "code": "serverError",
+    "message": "Server error"
+}
+
+SERVER_ERROR_404 = {
+    "http_code": 404,
+    "code": "notFound",
+    "message": "Resource not found"
+}
+
+UNAUTHORIZED_403 = {
+    "http_code": 403,
+    "code": "notAuthorized",
+    "message": "You are not allowed to do that."
+}
+
+NOT_FOUND_HANDLER_404 = {
+    "http_code": 404,
+    "code": "notFound",
+    "message": "There are no such handler"
+}
+
+SUCCESS_200 = {
+    'http_code': 200,
+    'code': 'success'
+}
+
+
+def response_with(response, value=None, message=None, error=None, headers={}, pagination=None):
+    result = {}
+    if value is not None:
+        result.update(value)
+
+    if message is not None:
+    	result.update(message);
+
+    # if response.get('message', None) is not None:
+    #     result.update({'message': response['message']})
+
+    result.update({'code': response['code']})
+    result.update({'http_code' : response['http_code']})
+
+    if error is not None:
+        result.update({'errors': error})
+
+    if pagination is not None:
+        result.update({'pagination': pagination})
+
+    headers.update({'Access-Control-Allow-Origin': '*'})
+    headers.update({'server': 'Flask Starter API'})
+
+    return make_response(jsonify(result), response['http_code'], headers)
